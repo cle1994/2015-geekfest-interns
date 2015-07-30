ff9b4cce7f7fe594b54c922d262bccea0e5f700f
Christian Le <christianle94@gmail.com>
2015-07-30 10:45:44 -0700

    init

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Christian Le on 7/30/15.
+//  Copyright (c) 2015 geekfest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
